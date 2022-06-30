2f00bab9c45caf83fa1adcd4410267cca8a5b2c6
gowthami-krishnan <108142949+gowthami-krishnan@users.noreply.github.com>
2022-06-30 13:12:01 +0530

    Add files via upload

---
+++ BankInformation.java
@@ -0,0 +1,16 @@
+package Week3;
+
+public class BankInformation {
+	
+	public void saving() {
+		System.out.println("Saving Account");
+	}
+	
+	public void fixed() {
+		System.out.println("Fixed Account");
+	}
+    
+	public void depoist() {
+		System.out.println("Depoist Account");
+	}
+}
